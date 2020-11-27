dcbbe29d346b100c767b6563e1ae90c28e54e918
Yonas Abebe <45671390+yonexo78@users.noreply.github.com>
2020-11-27 12:55:31 -0500

    These are the java project I am pushing to My github repo - YoniJavaEclipseRepo

---
+++ com.yoni.sum1/src/com/yoni/sum1/yoniSum.java
@@ -0,0 +1,15 @@
+package com.yoni.sum1; //Java Programmer ~ Yonas Abebe
+
+public class yoniSum {
+
+	public static void main(String[] args) {
+		int num1=8;
+		int num2=10;
+		int result;
+		System.out.println("Sum of: " +num1);
+		System.out.println("+: " +num2);
+		result= num1 + num2;
+		System.out.println("Equals: " +result);
+	}
+
+}
